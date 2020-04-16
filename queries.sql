abc675232c6c0758b78274cbef428156c8b8e497
Idong Essien <idong.essien@comcast.net>
2020-04-15 22:59:28 -0400

    Queries going well !

--- queries.sql
+++ queries.sql
@@ -6,11 +6,19 @@
 
 -- Find the phone number for the supplier with the id 11
 
+    select phone from suppliers where supplierid=11
+
 -- List first 10 orders placed, sorted descending by the order date
+
+    select * from orders order by orderdate desc limit 10
 
 -- Find all customers that live in London, Madrid, or Brazil
 
+    select * from customers where city="Madrid" or city="London" or country="Brazil"
+
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+
+    insert into customers (customername, contactname, address, city, postalcode, country) values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth')
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
